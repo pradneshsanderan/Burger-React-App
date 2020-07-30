22a040b3dc5791221c21ffc3614d094a65b2a18a
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-07-30 18:33:30 +0800

    handle errors and database

---
+++ burger/src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Modal from '../../components/UI/Modal/Modal';
+import Aux from '../Auxip/Auxilp';
+
+
+const withErrorHandler = (WrappedComponent, axios) => {
+    return class extends React.Component {
+        state={
+            error: null,
+        }
+
+        componentWillMount () {
+            axios.interceptors.request.use(req => {
+                this.setState({error: null});
+                return req;
+            })
+            axios.interceptors.response.use(res => res, error => {
+                this.setState({error: error});
+            })
+        }
+
+        errorConfirmedHandler = () => {
+            this.setState({error: null});
+        }
+        render () {
+            return (
+                <Aux>
+                    <Modal 
+                    clicked={this.errorConfirmedHandler}
+                    show={this.state.error}>
+                        {this.state.error ? this.state.error.message : null}
+                    </Modal>
+                <WrappedComponent {...this.props} />
+                </Aux>
+            );
+        }
+        
+    }
+
+}
+
+export default withErrorHandler;
