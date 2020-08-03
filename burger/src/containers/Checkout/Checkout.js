d9de56b91f7f8d2def1f19b0f77945d61bcac2f4
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-08-04 00:09:28 +0800

    passing data between pages

--- burger/src/containers/Checkout/Checkout.js
+++ burger/src/containers/Checkout/Checkout.js
@@ -3,22 +3,26 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 import { Route } from 'react-router-dom';
 import ContactData from './ContactData/ContactData';
 
+
 class Checkout extends Component {
     state={
-        ingredients:{
-            salad:1,
-            meat:1,
-            cheese:1,
-            bacon:1,
-        }
+        ingredients:null,
+        price: 0,
     }
-    componentDidMount(){
+    componentWillMount(){
         const query = new URLSearchParams(this.props.location.search);
         const ingredients = {};
+        let price = 0;
         for(let param of query.entries()){
+            if(param[0]=== 'price'){
+                price = param[1];
+            }else{
+
+            
             ingredient[param[0]] = +param[1];
+            }
         }
-        this.setState({ingredients: ingredients});
+        this.setState({ingredients: ingredients, totalPrice: price});
     }
     CheckoutCancelHandler = () => {
         this.props.history.goBack();
@@ -35,7 +39,7 @@ class Checkout extends Component {
                 CheckoutContinued={this.CheckoutContinueHandler}
                 ingredients={this.state.ingredients}/>
                 <Route path={this.props.match.path + '/contact-data'} component={ContactData}
-                redner={() => (<ContactData ingredient={this.state.ingredients} />)}/>
+                redner={(props) => (<ContactData ingredient={this.state.ingredients}  price ={this.state.totalPrice}/>)} {...props}/>
             </div>
         );
     }
