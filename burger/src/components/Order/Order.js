3082f3fad41e537985dce061903e7ddab6284776
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-08-04 00:31:15 +0800

    Orders Backend

---
+++ burger/src/components/Order/Order.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './Order.css';
+
+const order = (props) => (
+    <div className={classes.order}>
+        <p>Ingredients: Salad (1)</p>
+        <p>Price: <strogn>5.45</strogn></p>
+    </div>
+);
+
+export default order;
---
+++ burger/src/containers/Orders/Orders.js
@@ -0,0 +1,17 @@
+const { Component } = require("react");
+
+import React , { Component } from 'react';
+import Order from '../../components/Order/Order';
+
+class Order extends Component {
+    render () {
+        return (
+            <div>
+                <Order />
+                <Order />
+            </div>
+        );
+    }
+}
+
+export default Orders ;
