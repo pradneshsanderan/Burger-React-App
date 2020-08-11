5147694c9ed777c6192272559e5ccca03d256417
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-08-11 22:09:12 +0800

    new com

--- burger/src/containers/Checkout/Checkout.js
+++ burger/src/containers/Checkout/Checkout.js
@@ -2,28 +2,10 @@ import React, { Component } from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import { Route } from 'react-router-dom';
 import ContactData from './ContactData/ContactData';
+import { connect } from 'react-redux';
 
 
 class Checkout extends Component {
-    state={
-        ingredients:null,
-        price: 0,
-    }
-    componentWillMount(){
-        const query = new URLSearchParams(this.props.location.search);
-        const ingredients = {};
-        let price = 0;
-        for(let param of query.entries()){
-            if(param[0]=== 'price'){
-                price = param[1];
-            }else{
-
-            
-            ingredient[param[0]] = +param[1];
-            }
-        }
-        this.setState({ingredients: ingredients, totalPrice: price});
-    }
     CheckoutCancelHandler = () => {
         this.props.history.goBack();
     }
@@ -37,12 +19,18 @@ class Checkout extends Component {
                 <CheckoutSummary 
                 CheckoutCancel={this.CheckoutCancelHandler}
                 CheckoutContinued={this.CheckoutContinueHandler}
-                ingredients={this.state.ingredients}/>
+                ingredients={this.props.ings}/>
                 <Route path={this.props.match.path + '/contact-data'} component={ContactData}
-                redner={(props) => (<ContactData ingredient={this.state.ingredients}  price ={this.state.totalPrice}/>)} {...props}/>
+                component={ContactData}/>
             </div>
         );
     }
 }
 
-export default Checkout;
+const mapStateToProps =state => {
+ return{
+     ings: state.ingredients,
+ }
+};
+
+export default connect(mapStateToProps)( Checkout);
--- burger/src/containers/Checkout/ContactData/ContactData.js
+++ burger/src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,8 @@ import classes from './ContactData.css';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
+import {connect} from 'react-redux';
+import { stat } from 'fs-extra';
 
 class ContactData extends Component {
     state = {
@@ -104,8 +106,8 @@ class ContactData extends Component {
             formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
         }
         const order = {
-            ingredients: this.props.ingredients,
-            price: this.state.totalPrice,
+            ingredients: this.props.ings,
+            price: this.props.price,
             orderData: formData
 
         }
@@ -182,4 +184,11 @@ class ContactData extends Component {
     }
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients,
+        price: state.totalPrice
+    }
+};
+
+export default connect(mapStateToProps)(ContactData);
