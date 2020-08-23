026e31ab49ae7dbf23995f80caa169ac7f4ea03b
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-08-24 01:06:26 +0800

    adding a spinner

--- br/src/containers/Auth/Auth.js
+++ br/src/containers/Auth/Auth.js
@@ -4,7 +4,7 @@ import Button from '../../components/UI/Button/Button';
 import classes from './Auth.css';
 import * as actions from '../../store/actions/index';
 import { connect } from 'react-redux';
-
+import Spinner from '../../components/UI/Spinner/Spinner';
 class Auth extends C {
     state = {
         controls: {
@@ -44,7 +44,7 @@ class Auth extends C {
         if (!rules) {
             return true;
         }
-        
+
         if (rules.required) {
             isValid = value.trim() !== '' && isValid;
         }
@@ -70,24 +70,24 @@ class Auth extends C {
         return isValid;
     }
     inputChangedHandler = (event, controlName) => {
-        const updatedControls ={
+        const updatedControls = {
             ...this.state.controls,
-            [controlName]:{
+            [controlName]: {
                 ...this.state.controls[controlName],
                 value: event.target.value,
                 value: this.checkValidity(event.target.value, this.state.controls[controlName]).validation,
                 touched: true,
             }
         };
-        this.setState({controls: updatedControls});
+        this.setState({ controls: updatedControls });
     }
     submitHandler = (event) => {
         event.preventDefault();
-        this.props.onAuth(this.state.controls.email.value,this.state.controls.password.value, this.state.isSignUp );
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignUp);
     }
     switchAuthModeHandler = () => {
         this.setState(prevState => {
-            return {isSignUp: !prevState.isSignUp};
+            return { isSignUp: !prevState.isSignUp };
         })
     }
 
@@ -100,7 +100,7 @@ class Auth extends C {
                 config: this.state.orderForm[key]
             });
         }
-        const form = formElementsArray.map(formElement => (
+        let form = formElementsArray.map(formElement => (
             <Input key={formElement.id}
                 elementType={formElement.config.elementType}
                 elementConfig={formElement.config.elementConfig}
@@ -109,25 +109,43 @@ class Auth extends C {
                 shouldValidate={formElement.config.validation}
                 touched={formElement.config.touched}
                 changed={(event) => this.inputChangedHandler(event, formElement.id)} />
-            
-                ));
+
+        ));
+
+        if (this.props.loading) {
+            form = <Spinner />
+        }
+        const errorMessage = null;
+        if (this.props.error) {
+            errorMessage = (
+                <p>{this.props.error.message}</p>
+            );
+        }
         return (
             <div className={classes.Auth}>
-                <form onSubmit={this.submitHandler}> 
+                {errorMessage}
+                <form onSubmit={this.submitHandler}>
                     {form}
-                <Button btnType="Success">SUBMIT</Button>
+                    <Button btnType="Success">SUBMIT</Button>
                 </form>
-                <Button 
-                clicked={this.switchAuthModeHandler}
-                btnType="Danger">SWITCH TO {this.state.isSignUp ? 'SIGN IN' : 'SIGN UP'}</Button>
+                <Button
+                    clicked={this.switchAuthModeHandler}
+                    btnType="Danger">SWITCH TO {this.state.isSignUp ? 'SIGN IN' : 'SIGN UP'}</Button>
             </div>
         );
     }
 }
+const mapStateToProps = state => {
+    return {
+        loading: state.auth.loading,
+        error: state.auth.error
+
+    };
+};
 const mapDispatchToProps = dispatch => {
     return {
-        onAuth:(email,password, isSignUp) => dispatch(actions.auth(email,password,isSignUp))
+        onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp))
     };
 };
 
-export default connect(null,mapDispatchToProps) (Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
--- br/src/store/actions/auth.js
+++ br/src/store/actions/auth.js
@@ -4,45 +4,46 @@ import axios from 'axios';
 
 export const authStart = () => {
     return {
-        type:actionTypes.AUTH_START
+        type: actionTypes.AUTH_START
     };
 };
 
 export const authSuccess = (idToken, userId) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
-        token:idToken,
-        userId:userId
+        token: idToken,
+        userId: userId
+    };
 };
 
 
 export const authFail = (error) => {
-    return{
-        type:actionTypes.AUTH_FAIL,
-        error:error
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error: error
     };
 };
 
-export const auth = (email, password,isSignup) => {
-    return dispatchEvent =>{
+export const auth = (email, password, isSignup) => {
+    return dispatchEvent => {
         dispatchEvent(authStart());
         const authData = {
-            email:email,
-            password:password,
-            returnSecureToken:true
+            email: email,
+            password: password,
+            returnSecureToken: true
         };
-        let url ='https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyAscnE069mzy4Naffkya8i1pNdoEuJzK9o';
-        if(!isSignup){
-            url='https://identitytoolkit.googleapis.com/v1/accounts:verifyPassword?key=AIzaSyAscnE069mzy4Naffkya8i1pNdoEuJzK9o'
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyAscnE069mzy4Naffkya8i1pNdoEuJzK9o';
+        if (!isSignup) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:verifyPassword?key=AIzaSyAscnE069mzy4Naffkya8i1pNdoEuJzK9o'
         }
-        axios.post(url,authData)
-        .then(response => {
-            console.log(response);
-            dispatch(authSuccess(response.data.idToken, response.data.localId));
-        })
-        .catch( err => {
-            console.log(err);
-            dispatch(authFail(err));
-        })
+        axios.post(url, authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
+            })
+            .catch(err => {
+
+                dispatch(authFail(err.response.data.error));
+            })
     };
 };
