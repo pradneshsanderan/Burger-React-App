d4ff00a6a36bdc9b4347a8a525d05704f2529acc
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-08-24 17:35:49 +0800

    forward unauthenticated users

--- br/src/containers/Auth/Auth.js
+++ br/src/containers/Auth/Auth.js
@@ -5,6 +5,7 @@ import classes from './Auth.css';
 import * as actions from '../../store/actions/index';
 import { connect } from 'react-redux';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import { Redirect } from 'react-router-dom';
 class Auth extends C {
     state = {
         controls: {
@@ -121,8 +122,13 @@ class Auth extends C {
                 <p>{this.props.error.message}</p>
             );
         }
+        let authRedirect = null;
+        if(this.props.isAuthenticated){
+            authRedirect= <Redirect to="/"/>
+        }
         return (
             <div className={classes.Auth}>
+                {authRedirect}
                 {errorMessage}
                 <form onSubmit={this.submitHandler}>
                     {form}
@@ -138,7 +144,8 @@ class Auth extends C {
 const mapStateToProps = state => {
     return {
         loading: state.auth.loading,
-        error: state.auth.error
+        error: state.auth.error,
+        isAuthenticated: state.auth.token
 
     };
 };
--- br/src/containers/BurgerBuilder/BurgerBuilder.js
+++ br/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -37,7 +37,12 @@ class BurgerBuilder extends Component {
     }
 
     purchaseHandler = () => {
-        this.setState( { purchasing: true } );
+        if(this.props.isAuthenticated){
+            this.setState( { purchasing: true } );
+        }else{
+            this.props.history.push('/auth');
+        }
+        
     }
 
     purchaseCancelHandler = () => {
@@ -69,7 +74,8 @@ class BurgerBuilder extends Component {
                         disabled={disabledInfo}
                         purchasable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler}
-                        price={this.props.price} />
+                        price={this.props.price}
+                        isAuth={this.props.isAuthenticated} />
                 </Aux>
             );
             orderSummary = <OrderSummary
@@ -94,7 +100,8 @@ const mapStateToProps = state => {
     return {
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
-        error: state.burgerBuilder.error
+        error: state.burgerBuilder.error,
+        isAuthenticated : state.auth.token !== null
     };
 }
 
